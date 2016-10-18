8bcbe5a75fdbc3e8aeec77afdeebbf81b820f18e
Sean Perez <sean.perez29@gmail.com>
2016-10-17 18:51:40 -0700

    feat: Add functionality for view adjustment when showing and hiding the keyboard

--- GifMaker_Swift_Template/GifEditorViewController.swift
+++ GifMaker_Swift_Template/GifEditorViewController.swift
@@ -16,6 +16,7 @@ class GifEditorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        captionTextField.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,6 +31,8 @@ class GifEditorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
 
     /*
     // MARK: - Navigation
@@ -41,4 +44,45 @@ class GifEditorViewController: UIViewController {
     }
     */
 
+}
+
+extension GifEditorViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        captionTextField.placeholder = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        captionTextField.resignFirstResponder()
+        return true
+    }
+}
+
+extension GifEditorViewController {
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(GifEditorViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(GifEditorViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWillShow(notification: Notification) {
+        if view.frame.origin.y >= 0 {
+            view.frame.origin.y -= getKeyboardHeight(notification: notification)
+        }
+    }
+    
+    func keyboardWillHide(notification: Notification) {
+        if (self.view.frame.origin.y < 0) {
+            view.frame.origin.y += getKeyboardHeight(notification: notification)
+        }
+    }
+    
+    func getKeyboardHeight(notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
 }
